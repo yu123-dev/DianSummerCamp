8339f8652048f4361bab2ab3a471073f81221e77
yu123-dev <73051005+yu123-dev@users.noreply.github.com>
2021-08-03 22:03:35 +0800

    Delete forms.py
    useless file

--- tea/userprofile/forms.py
+++
@@ -1,9 +0,0 @@
-# 引入表单类
-from django import forms
-# 引入 User 模型
-from .models import User
-
-# 登录表单，继承了 forms.Form 类
-class UserLoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField()
